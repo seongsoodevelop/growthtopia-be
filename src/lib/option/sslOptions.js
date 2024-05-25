714249a884ba02f2160dddd28a1b59c758696368
seongsoodevelop <119916484+seongsoodevelop@users.noreply.github.com>
2024-05-25 21:53:07 +0900

    certificate location changed

--- src/lib/option/sslOptions.js
+++ src/lib/option/sslOptions.js
@@ -2,7 +2,7 @@ import fs from "fs";
 
 export const getSSLOptions = () => {
   return {
-    key: fs.readFileSync("./certificate/privkey.pem", "utf8").toString(),
-    cert: fs.readFileSync("./certificate/fullchain.pem", "utf8").toString(),
+    key: fs.readFileSync("../certificate/privkey.pem", "utf8").toString(),
+    cert: fs.readFileSync("../certificate/fullchain.pem", "utf8").toString(),
   };
 };
